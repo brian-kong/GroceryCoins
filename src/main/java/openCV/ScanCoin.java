6e034818e73fa8cee70b513c19d46e1c2873ba06
Brian Kong <briankong1212@hotmail.com>
2023-01-03 17:03:58 -0800

    Boilerplate code scanCoin, added pom.xml

---
+++ src/main/java/openCV/ScanCoin.java
@@ -0,0 +1,13 @@
+package openCV;
+
+import org.opencv.core.Mat;
+import java.util.ArrayList;
+
+public class ScanCoin {
+
+    public ArrayList<Integer> findCoins(Mat mat) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        return list;
+    }
+}
